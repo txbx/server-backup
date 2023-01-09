4cc725bdde53a8957428b13bf0e5933f5050e1e7
txbx <1660233887@qq.com>
2023-01-09 12:48:07 +0800

    解决日志中文问号问题

--- src/main/java/backup/App.java
+++ src/main/java/backup/App.java
@@ -15,6 +15,8 @@ public class App {
         Map<String, String> getenv = System.getenv();
         System.out.println("环境变量:" + getenv);
 
+        System.out.println("属性："+System.getProperties());
+
         // 启动就执行一次
         doBackUp();
 
